9fe4f75a14689ff4ba5ba36c7741f2bcd300086a
jum0624 <jum0624@naver.com>
2021-05-09 02:56:07 +0900

    style: JavaStudy Group

--- Eclipse IDE/Java06/src/JavaHw/CalendarEx.java
+++ Eclipse IDE/Java06/src/JavaHw/CalendarEx.java
@@ -8,8 +8,8 @@ public class CalendarEx {
         int month = cal.get(Calendar.MONTH) + 1;  //달(월)
         int day = cal.get(Calendar.DAY_OF_MONTH);    //일
         int hourOfDay = cal.get(Calendar.HOUR_OF_DAY);  //시(24시간)
-        int minute = cal.get(Calendar.MINUTE);
-        int second = cal.get(Calendar.SECOND);
+        int minute = cal.get(Calendar.MINUTE);  // 분
+        int second = cal.get(Calendar.SECOND);  // 초
 
         System.out.println(year + "년 " + month + "월 " + day + "일");
         System.out.println(hourOfDay + "시 " + minute + "분 " + second + "초");
--- Eclipse IDE/Java06/src/JavaHw/StringTokenizerEx.java
+++ Eclipse IDE/Java06/src/JavaHw/StringTokenizerEx.java
@@ -6,29 +6,26 @@ import java.util.StringTokenizer;
 public class StringTokenizerEx {
 	public static void main(String[] args) {
 		Scanner sc = new Scanner(System.in);
-		String s = sc.next();
+		String s = sc.next();  // 문자열 받기
 		
-		Vector<String> v = new Vector<String>();
+		Vector<String> v = new Vector<String>();  // 문자열 토큰 저장을 위한 벡터 생성
 		
-		StringTokenizer st = new StringTokenizer(s,"+-",true);
+		StringTokenizer st = new StringTokenizer(s,"+-",true); // 문자열 쪼개기
 		while(st.hasMoreTokens()) {
-			v.add(st.nextToken());
+			v.add(st.nextToken());  // 문자열 토큰 벡터에 저장
 		}
 		
-		int sum = Integer.parseInt(v.elementAt(0));
-		//System.out.println("+:"+sum);
-		int i = 1;
-		while(i < v.size()) {
-			String n = v.elementAt(i);
+		int sum = Integer.parseInt(v.elementAt(0)); //벡터의 첫번째 정수화 후, 연산
+		int i = 1;  // 벡터의 0번쨰를 우선 계산 했기 때문에 다음 문자열부터 시작함.
+		while(i < v.size()) {  // i가 벡터의 크기보다 작을 때까지 반복문 진행
+			String n = v.elementAt(i); // 덧셈,뺄셈 연산자 저장
 			if(n.equals("+")) {
-				sum += Integer.parseInt(v.elementAt(i+1));
-				//System.out.println("+:"+sum);
+				sum += Integer.parseInt(v.elementAt(i+1)); // 벡터의 해당 요소가 +일 때, 다음 요소 덧셈 연산
 			}
 			else if(n.equals("-")) {
-				sum -= Integer.parseInt(v.elementAt(i+1));
-				//System.out.println("-:"+sum);
+				sum -= Integer.parseInt(v.elementAt(i+1)); // 벡터의 해당 요소가 -일 때, 다음 요소 뺄셈 연산
 			}
-			i += 2;
+			i += 2; // n에 연산자만 넣기 위해 2씩 증가(불필요한 연산 제거)
 		}
 		
 		System.out.println(sum);
